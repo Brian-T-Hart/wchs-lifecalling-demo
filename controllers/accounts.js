bbfe5d340a7aa1257de41825aa6f4545a698c16d
Brian-T-Hart <hart.briant@gmail.com>
2019-06-28 09:57:26 -0700

    replaced bcrypt-nodejs with bcrypt

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -1,79 +1,85 @@
-var bcrypt = require('bcrypt-nodejs');
-var express = require("express");
-var passport = require("passport");
-var router = express.Router();
-var db = require("../models");
-
+const bcrypt = require("bcrypt");
+const db = require("../models");
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
 
 // ALL links in this file get prepended with /account
 // ==================================================
 
 // this is handling the login authentication
-router.post('/login',
-	passport.authenticate('local', { session: false }),
-	function (req, res) {
-		console.log("user authenticated..account.routes.js ", req.user.username, req.user.token, req.user.isAdmin);
-		res.json({ username: req.user.username, token: req.user.token, isAdmin: req.user.isAdmin });
-	});
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  function(req, res) {
+    console.log(
+      "user authenticated..account.routes.js ",
+      req.user.username,
+      req.user.token,
+      req.user.isAdmin
+    );
+    res.json({
+      username: req.user.username,
+      token: req.user.token,
+      isAdmin: req.user.isAdmin
+    });
+  }
+);
 
 // this is handling the registration
-router.post('/register', passport.authenticate('local-register', {
-	successRedirect: '/login',
-	failureRedirect: '/'
-}));
+router.post(
+  "/register",
+  passport.authenticate("local-register", {
+    successRedirect: "/login",
+    failureRedirect: "/"
+  })
+);
 
 // this is handling the password reset
-router.post('/passwordReset', (req, res, next) => {
-	db.students.findOne(
-		{
-			attributes: ['isAdmin', 'password'],
-			where:
-				{
-					id: req.body.id
-				}
-		}
-	)
-	.then(
-		function (results) {
-			console.log("adminPassword: ", req.body.adminPassword);
-			console.log("isAdmin: ", results.isAdmin);
-			console.log("password sync: ", bcrypt.compareSync(req.body.adminPassword, results.password));
-			if (!results.isAdmin) {
-				res.json("You do not have admin privileges!");
-			}
-
-			else if (!bcrypt.compareSync(req.body.adminPassword, results.password)) {
-				res.json("The admin password is incorrect");
-			}
-
-			else {
-				db.students.update(
-					{
-						password: bcrypt.hashSync(req.body.password)
-					},
-					{
-						where:
-						{
-							email: req.body.email
-						}
-					}
-				)
-				.then(
-					function (results) {
-						res.json(results);
-					}
-				)
-				.catch(
-					function (err) {
-						console.log(err);
-					}
-				)
-			}
-		}
-	)
-	.catch(function (err) {
-		console.log(err);
-	})
+router.post("/passwordReset", (req, res, next) => {
+  db.students
+    .findOne({
+      attributes: ["isAdmin", "password"],
+      where: {
+        id: req.body.id
+      }
+    })
+    .then(function(results) {
+      console.log("adminPassword: ", req.body.adminPassword);
+      console.log("isAdmin: ", results.isAdmin);
+      console.log(
+        "password sync: ",
+        bcrypt.compareSync(req.body.adminPassword, results.password)
+      );
+      if (!results.isAdmin) {
+        res.json("You do not have admin privileges!");
+      } else if (
+        !bcrypt.compareSync(req.body.adminPassword, results.password)
+      ) {
+        res.json("The admin password is incorrect");
+      } else {
+        db.students
+          .update(
+            {
+              password: bcrypt.hashSync(req.body.password)
+            },
+            {
+              where: {
+                email: req.body.email
+              }
+            }
+          )
+          .then(function(results) {
+            res.json(results);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 });
 
 module.exports = router;
--- passport.js
+++ passport.js
@@ -1,59 +1,72 @@
-var passport = require('passport');
-var bcrypt = require('bcrypt-nodejs');
-const LocalStrategy = require('passport-local').Strategy
-var db = require('./models');
-const config = require('./config');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const config = require("./config");
+const db = require("./models");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const saltRounds = 10;
 
 const authenticate = (username, password, done) => {
-	db.students.find({
-		where: {
-			username: username
-		}
-	}).then(student => {
-		if (!student || !bcrypt.compareSync(password, student.password)) {
-			console.log("failed to login, username not found.");
-			return done(null, false, { message: 'invalid username/or and password combination' });
-		}
-		const payload = {
-			sub: student.id
-		};
+  db.students
+    .find({
+      where: {
+        username: username
+      }
+    })
+    .then(student => {
+      if (!student || !bcrypt.compareSync(password, student.password)) {
+        console.log("failed to login, username not found.");
+        return done(null, false, {
+          message: "invalid username/or and password combination"
+        });
+      }
+      const payload = {
+        sub: student.id
+      };
 
-		// create a token string
-		const token = jwt.sign(payload, config.jwtSecret);
-		const data = {
-			isAdmin: student.isAdmin,
-			username: student.username,
-			token: token
-		};
-		return done(null, data);
-	})
-		.catch((err) => done(null, err)) // pass the error back
-}
+      // create a token string
+      const token = jwt.sign(payload, config.jwtSecret);
+      const data = {
+        isAdmin: student.isAdmin,
+        username: student.username,
+        token: token
+      };
+      return done(null, data);
+    })
+    .catch(err => done(null, err)); // pass the error back
+};
 
 const register = (req, email, password, done) => {
-	//sequelize?
-	db.students.find({
-		where: {
-			username: req.body.username,
-		}
-	})
-		.then(user => {
-			if (user) {
-				return done(null, false, { message: 'an account with that email has already been created' });
-			}
-			if (password !== req.body.password) {
-				return done(null, false, { message: `passwords don't match` });
-			}
-			db.students.create({
-				username: req.body.username,
-				email: req.body.email,
-				password: bcrypt.hashSync(password)
-			}).then(function () {
-				done(null, user);
-			})
-		})
-}
+  //sequelize?
+  db.students
+    .find({
+      where: {
+        username: req.body.username
+      }
+    })
+    .then(user => {
+      if (user) {
+        return done(null, false, {
+          message: "an account with that email has already been created"
+        });
+      }
+      if (password !== req.body.password) {
+        return done(null, false, { message: `passwords don't match` });
+      }
+      db.students
+        .create({
+          username: req.body.username,
+          email: req.body.email,
+          password: bcrypt.hashSync(password, saltRounds)
+        })
+        .then(function() {
+          done(null, user);
+        });
+    });
+};
 
 passport.use(new LocalStrategy(authenticate));
-passport.use('local-register', new LocalStrategy({ passReqToCallback: true }, register));
+passport.use(
+  "local-register",
+  new LocalStrategy({ passReqToCallback: true }, register)
+);
