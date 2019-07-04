fc6dfb93a6a8a69a5fff3abb245e84b85222c48a
Brian-T-Hart <hart.briant@gmail.com>
2019-07-03 21:05:23 -0700

    added search feature

--- client/src/components/AllStudents/AllStudents.js
+++ client/src/components/AllStudents/AllStudents.js
@@ -8,9 +8,11 @@ class AllStudents extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			allStudents: []
+			allStudents: [],
+			studentsToDisplay: [],
+			searchInput: ''
+		}
 	}
-}
 
 	componentWillMount() {
 
@@ -18,92 +20,118 @@ class AllStudents extends Component {
 			id: localStorage.getItem("lifeCallingId"),
 			username: localStorage.getItem("lifeCallingUsername")
 		})
-		.then(res => {
-			var students = res.data;
-			var data = [];
-			for (var i = 0; i < students.length; i++) {
-				data[i] = students[i]
-			}
-			this.setState({allStudents: data});
-		})
-		.catch(err => {
-			console.log(err);
-		})
+			.then(res => {
+				var students = res.data;
+				var data = [];
+				for (var i = 0; i < students.length; i++) {
+					data[i] = students[i]
+				}
+				this.setState(
+					{
+						allStudents: data,
+						studentsToDisplay: data
+					}
+				);
+			})
+			.catch(err => {
+				console.log(err);
+			})
 	}
+
+	handleInputChange = (event) => {
+		const { name, value } = event.target;
+		this.setState({
+			[name]: value
+		});
+
+		let students = this.state.allStudents.filter(student => student.email.includes(value));
+		this.setState({ studentsToDisplay: students });
+	};
 
 	render() {
 
-		const allTheStudents = this.state.allStudents.map((student) =>
-				<tr className="studentInfoRow" key={student.id}>
-					<td>{student.username}</td>
-					<td>{student.email}</td>
+		const allTheStudents = this.state.studentsToDisplay.map((student) =>
+			<tr className="studentInfoRow" key={student.id}>
+				<td>{student.username}</td>
+				<td>{student.email}</td>
 
-					<td>
-						{student.strengths != null ? 
-							<ul>
-								{student.strengths.map((myStrength) => <li key={myStrength}>{myStrength}</li>)}
-							</ul> : ""}
-					</td>
+				<td>
+					{student.strengths != null ?
+						<ul>
+							{student.strengths.map((myStrength) => <li key={myStrength}>{myStrength}</li>)}
+						</ul> : ""}
+				</td>
 
-					<td>
-						{student.mbti != null ? 
-							<ul>
-								{student.mbti.map((type) => <li key={type}>{type}</li>)}
-							</ul> : ""}
-					</td>
+				<td>
+					{student.mbti != null ?
+						<ul>
+							{student.mbti.map((type) => <li key={type}>{type}</li>)}
+						</ul> : ""}
+				</td>
 
-					<td>
-						{student.gifts != null ? 
-							<ul>
-								{student.gifts.map((gift) => <li key={gift}>{gift}</li>)}
-							</ul> : ""}
-					</td>
+				<td>
+					{student.gifts != null ?
+						<ul>
+							{student.gifts.map((gift) => <li key={gift}>{gift}</li>)}
+						</ul> : ""}
+				</td>
 
-					<td>
-						{student.loveLanguages != null ? 
-							<ul>
-								{student.loveLanguages.map((loveLanguage) => <li key={loveLanguage}>{loveLanguage}</li>)}
-							</ul> : ""}
-					</td>
+				<td>
+					{student.loveLanguages != null ?
+						<ul>
+							{student.loveLanguages.map((loveLanguage) => <li key={loveLanguage}>{loveLanguage}</li>)}
+						</ul> : ""}
+				</td>
 
-					<td>
-						{student.intelligences != null ? 
-							<ul>
-								{student.intelligences.map((intelligence) => <li key={intelligence}>{intelligence}</li>)}
-							</ul> : ""}
-					</td>
+				<td>
+					{student.intelligences != null ?
+						<ul>
+							{student.intelligences.map((intelligence) => <li key={intelligence}>{intelligence}</li>)}
+						</ul> : ""}
+				</td>
 
-					<td>
-						<a href={student.learningStylesUrl}>Learning Styles</a><br></br>
-						<a href={student.johariUrl}>Johari Window</a><br></br>
-						<a href={student.documentUrl}>CV</a>
-					</td>
+				<td>
+					<a href={student.learningStylesUrl}>Learning Styles</a><br></br>
+					<a href={student.johariUrl}>Johari Window</a><br></br>
+					<a href={student.documentUrl}>CV</a>
+				</td>
 
-					<td>
-						<Link to="/passwordReset">edit password</Link>
-					</td>
-				</tr>
+				<td>
+					<Link to="/passwordReset">edit password</Link>
+				</td>
+			</tr>
 		)
 
 		return (
-			<table className="content-container table all-students">
-				<thead>
-					<tr>
-					<th scope="col">Username</th>
-					<th scope="col">Email</th>
-					<th scope="col">Strengths</th>
-					<th scope="col">Myers-Briggs</th>
-					<th scope="col">Spiritual Gifts</th>
-					<th scope="col">Love Languages</th>
-					<th scope="col">Multiple-Intelligences</th>
-					<th scope="col">Documents</th>
-					</tr>
-				</thead>
+			<div>
+				<div className="align-items-center bg-light justify-content-center d-flex pb-2 pt-2" id="search-container">
+					<form className="form-inline pl-2">
+						<input className="form-control mb-0 text-center" name="searchInput" onChange={this.handleInputChange} placeholder="Search by Email" aria-label="Search" value={this.state.searchInput} />
+					</form>
+				</div>
 
-				<tbody>
-					{ allTheStudents }
-				</tbody>
-			</table>
+				<div className="content-container">
+					<table className="table all-students">
+						<thead>
+							<tr>
+								<th scope="col">Username</th>
+								<th scope="col">Email</th>
+								<th scope="col">Strengths</th>
+								<th scope="col">Myers-Briggs</th>
+								<th scope="col">Spiritual Gifts</th>
+								<th scope="col">Love Languages</th>
+								<th scope="col">Multiple-Intelligences</th>
+								<th scope="col">Documents</th>
+								<th scope="col"></th>
+							</tr>
+						</thead>
+
+						<tbody>
+							{allTheStudents}
+						</tbody>
+					</table>
+				</div>
+			</div >
 		)
 	}
 }
